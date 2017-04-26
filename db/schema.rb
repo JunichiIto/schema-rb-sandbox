571647e667a1977b11a5c222f7d62012b75b39df
Junichi Ito <jit@sonicgarden.jp>
2017-04-26 12:23:51 +0900

    Add author_name to blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426032205) do
+ActiveRecord::Schema.define(version: 20170426032325) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "author_name"
   end
 
 end
